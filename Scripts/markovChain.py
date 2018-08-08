88fd90a0d640d7c92b6ed23365471e31ba1679c2
Emily Brunelli <41587700+27182mily@users.noreply.github.com>
2018-08-08 08:56:23 -0700

    Update markovChain.py

--- Scripts/markovChain.py
+++ Scripts/markovChain.py
@@ -4,8 +4,13 @@ from aminoAcid import aminoAcids
 # aminoAcids should be an ordered list of strings, each string being the entire set
 # of basepairs in a single line. Then main(aminoAcids)[k][a][b] is the probability
 # of the k-th amino acid site's chance of mutating from acid a to acid b.
+
+
 def markov_chain(aminoAcids):
     #aminoAcids = ['ACCFSDKELA','ACCSEDKEFA','ACCREGKRFA','AEERCGKRFF'] only used as example
+    letterToNumber = {'A': 0, 'C': 1, 'D': 2, 'E': 3, 'F': 4, 'G': 5, 'H': 6, 'I': 7, 'K': 8, 'L': 9, 'M': 10, 'N': 11,
+                      'P': 12, 'Q': 13, 'R': 14, 'S': 15, 'T': 16, 'V': 17, 'W': 18, 'Y': 19}
+
     j = len(aminoAcids[0])
 
     transitionArray = []
@@ -13,25 +18,31 @@ def markov_chain(aminoAcids):
     for k in range(j):
         aminoAcidTransition = {}
         for i in range(len(aminoAcids)-1):
+
             transition = aminoAcids[i][k] + aminoAcids[i+1][k]
-            if transition in aminoAcidTransition:
-                aminoAcidTransition[transition] += 1
+
+            if aminoAcids[i][k] == '-':
+                for key in letterToNumber:
+                    aminoAcidTransition[key + aminoAcids[i+1][k]] = aminoAcidTransition.setdefault(key + aminoAcids[i+1][k],0) + .05
+            elif aminoAcids [i+1][k] == '-':
+                for key in letterToNumber:
+                    aminoAcidTransition[aminoAcids[i][k] + key] = aminoAcidTransition.setdefault(aminoAcids[i][k] + key,0) + .05
+
             else:
-                aminoAcidTransition[transition] = 1
+                aminoAcidTransition[transition] = aminoAcidTransition.setdefault(transition, 0) + 1
+
         transitionArray.append(aminoAcidTransition)
 
-    letterToNumber = {'A':0, 'C':1, 'D':2, 'E':3, 'F':4, 'G':5, 'H':6, 'I':7, 'K':8, 'L':9, 'M':10, 'N':11,
-    'P':12, 'Q':13, 'R':14, 'S':15, 'T':16, 'V':17, 'W':18, 'Y':19, '-':20}
 
-    markovMatrices = np.zeros((j,21,21)) #creates a 21x21xnumberSites matrix, or 21x21x10 matrix in this case
+    markovMatrices = np.zeros((j,20,20)) #creates a 20x20xnumberSites matrix, or 20x20x10 matrix in this case
 
     for site in range(j): #for each amino acid site in a sequence
 
         for key in transitionArray[site]:
            markovMatrices[site][letterToNumber[key[0]]][letterToNumber[key[1]]] = transitionArray[site][key]  #first letter of aa, ab, etc.
 
-        markovMatrices = markovMatrices[:, :20, :20]
-        
+        #markovMatrices = markovMatrices[:, :20, :20]
+        #print(transitionArray[site])
         for z in range(20):
             sumRow = np.sum(markovMatrices[site][z])
             if sumRow != 0:
@@ -39,11 +50,11 @@ def markov_chain(aminoAcids):
             else:
                 markovMatrices[site][z] = markovMatrices[site][z]
 
-        #print(markovMatrices[site]) prints out 20x20 matrix for each site
+        #print(markovMatrices[site])
     return markovMatrices
 
 
-markov_chain(['ACCFSDKELA', 'CCCSEDKEFA', '-CCREGKRFA', 'AEERCGKRFF'])
+(markov_chain(['ACCFSDKELA', 'CCCSEDKEFA', '-CCREGKRFA', 'AEERCGKRFF']))
 
 # Note that you must print out a site's matrix by itself to view its entire contents,
 # i.e. print(main(...)[0])
