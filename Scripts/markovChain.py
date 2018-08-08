8ca6d94cd3801914580a4b1811c739006ad13ebc
Emily Brunelli <41587700+27182mily@users.noreply.github.com>
2018-08-07 19:32:46 -0700

    Accounts for dash

--- Scripts/markovChain.py
+++ Scripts/markovChain.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from aminoAcid import aminoAcids
 # Returns a 20x20xnumberOfSites Markov transition matrix using the given aminoAcids.
 # aminoAcids should be an ordered list of strings, each string being the entire set
 # of basepairs in a single line. Then main(aminoAcids)[k][a][b] is the probability
@@ -7,6 +7,7 @@ import numpy as np
 def markov_chain(aminoAcids):
     #aminoAcids = ['ACCFSDKELA','ACCSEDKEFA','ACCREGKRFA','AEERCGKRFF'] only used as example
     j = len(aminoAcids[0])
+
     transitionArray = []
 
     for k in range(j):
@@ -20,14 +21,17 @@ def markov_chain(aminoAcids):
         transitionArray.append(aminoAcidTransition)
 
     letterToNumber = {'A':0, 'C':1, 'D':2, 'E':3, 'F':4, 'G':5, 'H':6, 'I':7, 'K':8, 'L':9, 'M':10, 'N':11,
-    'P':12, 'Q':13, 'R':14, 'S':15, 'T':16, 'V':17, 'W':18, 'Y':19}
+    'P':12, 'Q':13, 'R':14, 'S':15, 'T':16, 'V':17, 'W':18, 'Y':19, '-':20}
 
-    markovMatrices = np.zeros((j,20,20)) #creates a 20x20xnumberSites matrix, or 20x20x10 matrix in this case
+    markovMatrices = np.zeros((j,21,21)) #creates a 20x20xnumberSites matrix, or 20x20x10 matrix in this case
 
     for site in range(j): #for each amino acid site in a sequence
+
         for key in transitionArray[site]:
            markovMatrices[site][letterToNumber[key[0]]][letterToNumber[key[1]]] = transitionArray[site][key]  #first letter of aa, ab, etc.
 
+        markovMatrices = markovMatrices[:, :20, :20]
+        
         for z in range(20):
             sumRow = np.sum(markovMatrices[site][z])
             if sumRow != 0:
@@ -35,11 +39,11 @@ def markov_chain(aminoAcids):
             else:
                 markovMatrices[site][z] = markovMatrices[site][z]
 
+        #print(markovMatrices[site]) prints out n 20x20 arrays, n being number of sites
     return markovMatrices
 
 
-# Example usage
-print(markov_chain(['ACCFSDKELA', 'ACCSEDKEFA', 'ACCREGKRFA', 'AEERCGKRFF']))
+markov_chain(['ACCFSDKELA', 'CCCSEDKEFA', '-CCREGKRFA', 'AEERCGKRFF'])
+
 # Note that you must print out a site's matrix by itself to view its entire contents,
 # i.e. print(main(...)[0])
-
